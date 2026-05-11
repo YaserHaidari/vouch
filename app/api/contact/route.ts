ad49bf435d33395b22da4a253c31f6cd2e23a295
Yaser Haidari <104608220@student.swin.edu.au>
2026-05-11 16:17:56 +1000

    contact us form working fine now but need to fix it more now, just did a demo test

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,14 +1,33 @@
+import { supabase } from "@/utils/supabase/client";
 import { NextResponse } from "next/server";
 
+type Body = {
+  first_name: string,
+  last_name: string,
+  email: string,
+  subject: string,
+  message: string,
+}
 export async function POST(request: Request) {
-  try {
-    const body = await request.json();
-    
-    // Process your data (e.g., send an email or save to DB)
-    console.log("Form data received:", body);
 
-    return NextResponse.json({ message: "Success" }, { status: 200 });
+  try {
+    const body: Body = await request.json()
+    const {first_name, last_name, email, subject, message} = body
+
+
+    const { error} = await supabase.from('contact_submissions').insert({first_name:first_name, last_name:last_name, message: message, email: email, subject: subject})
+    return NextResponse.json({message: 'Success'}, {status: 200})
   } catch (error) {
-    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+    return NextResponse.json({message: 'Invalid request'}, {status: 400})
   }
+  // try {
+  //   const body = await request.json();
+    
+  //   // Process your data (e.g., send an email or save to DB)
+  //   console.log("Form data received:", body);
+
+  //   return NextResponse.json({ message: "Success" }, { status: 200 });
+  // } catch (error) {
+  //   return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+  // }
 }
